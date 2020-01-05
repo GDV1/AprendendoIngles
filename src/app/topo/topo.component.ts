7822b3ab19d31b5ca0334d9a50861b5ee1dd1d88
GDV1 <gabrieldv.dv@hotmail.com>
2020-01-04 23:56:56 -0300

    Implementação de String Interpolation e Property Binding

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
+  titulo: string = 'Aprendendo Inglês';
   constructor() { }
 
   ngOnInit() {
